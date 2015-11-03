fd08488a1dd85cffc375238f8774fb43dc1f4964
Superstar <superstar@Superstars-Mac.local>
2015-11-02 23:30:34 -0800

    Initial Commit

---
+++ Goggle Ball/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Goggle Ball
+//
+//  Created by Superstar on 11/2/15.
+//  Copyright Superstar 2015. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Goggle Ball/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Goggle Ball
+//
+//  Created by Superstar on 11/2/15.
+//  Copyright Superstar 2015. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
